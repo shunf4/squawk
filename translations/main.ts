746fdef013ad7c83c15280176311a47a70323259
blue <blue@macaw.me>
2019-10-04 18:12:08 +0300

    first translations experiments

---
+++ translations/main.ts
@@ -0,0 +1,283 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Account</name>
+    <message>
+        <location filename="ui/widgets/account.ui" line="14"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="127"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="40"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="129"/>
+        <source>Your account login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="47"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="131"/>
+        <source>Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="54"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="133"/>
+        <source>A server address of your account. Like 404.city or macaw.me</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="61"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="135"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="68"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="136"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="75"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="138"/>
+        <source>Password of your account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="94"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="142"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="101"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="144"/>
+        <source>Just a name how would you call this account, doesn&apos;t affect anything</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="108"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="146"/>
+        <source>Resource</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="115"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="148"/>
+        <source>A resource name like &quot;Home&quot; or &quot;Work&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/account.ui" line="118"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_account.h" line="150"/>
+        <source>QXmpp</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Accounts</name>
+    <message>
+        <location filename="ui/widgets/accounts.ui" line="14"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_accounts.h" line="108"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/accounts.ui" line="45"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_accounts.h" line="109"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/accounts.ui" line="86"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_accounts.h" line="110"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/accounts.ui" line="96"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_accounts.h" line="111"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/accounts.ui" line="106"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_accounts.h" line="112"/>
+        <source>Change password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/accounts.ui" line="129"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_accounts.h" line="113"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>JoinConference</name>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="14"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="116"/>
+        <source>Join new conference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="22"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="117"/>
+        <source>JID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="29"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="119"/>
+        <source>Room JID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="32"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="121"/>
+        <source>identifier@conference.server.org</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="39"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="122"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="49"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="123"/>
+        <source>Join on login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="56"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="125"/>
+        <source>If checked Squawk will try to join this conference on login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="66"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="128"/>
+        <source>Nick name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="73"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="130"/>
+        <source>Your nick name for that conference. If you leave this field empty your account name will be used as a nick name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/joinconference.ui" line="76"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_joinconference.h" line="132"/>
+        <source>John</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewContact</name>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="14"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="103"/>
+        <source>Add new contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="22"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="104"/>
+        <source>Account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="29"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="106"/>
+        <source>An account that is going to have new contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="36"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="108"/>
+        <source>JID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="43"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="110"/>
+        <source>Jabber id of your new contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="46"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="112"/>
+        <source>name@server.dmn</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="53"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="113"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="60"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="115"/>
+        <source>The way this new contact will be labeled in your roster (optional)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/widgets/newcontact.ui" line="63"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_newcontact.h" line="117"/>
+        <source>John Smith</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Squawk</name>
+    <message>
+        <location filename="ui/squawk.ui" line="14"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="130"/>
+        <source>squawk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/squawk.ui" line="75"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="136"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/squawk.ui" line="81"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="137"/>
+        <source>Squawk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/squawk.ui" line="96"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="131"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/squawk.ui" line="105"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="132"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/squawk.ui" line="117"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="133"/>
+        <source>Add contact</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/squawk.ui" line="128"/>
+        <location filename="build/ui/squawkUI_autogen/include/ui_squawk.h" line="134"/>
+        <source>Add conference</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
