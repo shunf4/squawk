e7be046e9f5af7eba6f33bc77a75db5c53ee0991
blue <blue@macaw.me>
2019-10-20 22:39:11 +0300

    vcard ui file

---
+++ ui/widgets/vcard.cpp
@@ -0,0 +1,31 @@
+/*
+ * Squawk messenger. 
+ * Copyright (C) 2019  Yury Gubich <blue@macaw.me>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "vcard.h"
+#include "ui_vcard.h"
+
+VCard::VCard(QWidget* parent):
+    QWidget(parent),
+    m_ui(new Ui::VCard())
+{
+    m_ui->setupUi(this);
+}
+
+VCard::~VCard()
+{
+}
